6d02e155c45f1e0cc117e2c1154694ad7faf4251
MercedesBanales <mercibanales@hotmail.com>
2024-11-11 12:20:51 -0300

    fixed mongo

--- FullStackLI/my-blog-backend/src/db.js
+++ FullStackLI/my-blog-backend/src/db.js
@@ -1,9 +1,23 @@
 import { MongoClient } from 'mongodb';
 
+// [{
+//     name: 'learn-react',
+//     upvotes: 0,
+//     comments: [],
+// }, {
+//     name: 'learn-node',
+//     upvotes: 0,
+//     comments: [],
+// }, {
+//     name: 'mongodb',
+//     upvotes: 0,
+//     comments: [],
+// }]
+
 let db;
 
 async function connectToDb(cb) {
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    const client = new MongoClient('mongodb://127.0.0.1:27018');
     await client.connect();
 
     db = client.db('react-blog-db');
